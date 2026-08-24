ec029e9b36bc4d0c3775d47ccc8a2e0d1ad83cfa
Naveen-qae <134367753+Naveen-qae@users.noreply.github.com>
2026-08-24 10:57:13 +0530

    Add files via upload

---
+++ primitivedataTypes.js
@@ -0,0 +1,25 @@
+var phoneNumber =9362321414
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+
+var empName ="Naveen"
+console.log(empName);
+console.log(typeof empName);
+
+var  hasPlaywright= false
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+var userName
+console.log(userName);
+
+var shop =null
+console.log(typeof shop);
+
+
+
+
+
+
+
+
