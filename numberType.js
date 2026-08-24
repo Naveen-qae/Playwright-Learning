a98298a8e1acb71105f9b5f16e57d88015769b58
Naveen-qae <134367753+Naveen-qae@users.noreply.github.com>
2026-08-24 11:07:06 +0530

    Add files via upload

---
+++ numberType.js
@@ -0,0 +1,14 @@
+function numberType() {
+    var number =-11
+    if (number>0) {
+        console.log("The given number is positive")        
+    }
+    else if (number<0) {
+       console.log("The given number is negative") 
+         }
+         else{
+          console.log("The given number is Zero")  
+         }
+    
+}
+numberType()
